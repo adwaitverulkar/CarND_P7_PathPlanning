5f8c4485167299d1aac78adb4adc898a9ff7a3f8
adwaitverulkar <adwaitverulkar@vt.edu>
2020-08-20 17:05:42 -0400

    Added behavior for slowing down

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ int main() {
   int curr_lane = 1;
 
   // Reference velocity
-  double ref_vel = 49.5; // mph
+  double ref_vel = 0; // mph
 
   // Train a GNB classifier
   vector<vector<double>> X_train = Load_State("../gnb_pred_data/train_states.txt");
@@ -94,7 +94,7 @@ int main() {
   cout << "GNB Accuracy: " << (100*fraction_correct) << "%\n" << endl;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy,&curr_lane,&ref_vel]
+               &map_waypoints_dx,&map_waypoints_dy,&curr_lane,&ref_vel,&gnb]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -132,13 +132,35 @@ int main() {
           auto sensor_fusion = j[1]["sensor_fusion"];
 
           int prev_size = previous_path_x.size();
+          bool too_close = false;
 
           // START BEHAVIOR PLANNER
 
-          // 
-
           if(prev_size > 0)
             car_s = end_path_s;
+
+          // find ref velocity to use
+          for(int i = 0; i < sensor_fusion.size(); ++i) {
+            // car is in my lane
+            float d = sensor_fusion[i][6];
+            if(d < (2 + 4 * curr_lane + 2) && d > (2 + 4 * curr_lane - 2)) {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx + vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += ((double)prev_size * 0.02 * check_speed);
+              // Car should be in front and away by at max 30 m
+              if((check_car_s > car_s) && ((check_car_s-car_s) < 30)) {
+                too_close = true;
+              }
+            }
+          }
+          if(too_close) {
+            ref_vel -= .224;
+          } else if(ref_vel < 49.5) {
+            ref_vel += .224;
+          }
           
           // END BEHAVIOR PLANNER
 
